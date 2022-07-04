f7a0a09645303a6b0ae1a8eabd91b1369f0553bf
Alexandra <sashika25@mail.ru>
2022-07-04 11:46:16 +0300

    loading data from csv files

--- src/main/java/ru/internship/perepichka/entity/Task.java
+++ src/main/java/ru/internship/perepichka/entity/Task.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -16,8 +17,25 @@ import javax.persistence.*;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String header;
+
+    private String description;
+
+    @Temporal(TemporalType.DATE)
+    private Date deadline;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public enum Status {
+        NEW, IN_PROGRESS, DONE
+    }
 
 }
---
+++ src/main/java/ru/internship/perepichka/exception/DataLoadingException.java
@@ -0,0 +1,7 @@
+package ru.internship.perepichka.exception;
+
+public class DataLoadingException extends RuntimeException{
+    public DataLoadingException(String message) {
+        super(message);
+    }
+}
