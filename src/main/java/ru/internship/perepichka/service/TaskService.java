d37ed5d3c6e07723553c5c157adb917ced883726
Alexandra <sashika25@mail.ru>
2022-07-04 18:26:26 +0300

    adding task

--- src/main/java/ru/internship/perepichka/service/TaskService.java
+++ src/main/java/ru/internship/perepichka/service/TaskService.java
@@ -3,19 +3,28 @@ package ru.internship.perepichka.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.internship.perepichka.dao.TaskRepository;
+import ru.internship.perepichka.entity.Task;
+import ru.internship.perepichka.init.DataInit;
+
+import java.util.List;
 
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final EmployeeService employeeService;
+    private final DataInit dataInit;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, EmployeeService employeeService, DataInit dataInit) {
         this.taskRepository = taskRepository;
+        this.employeeService = employeeService;
+        this.dataInit = dataInit;
     }
 
-    public void addTask(String args){
-        throw new UnsupportedOperationException();
+    public List<Task> addTask(String args){
+        taskRepository.save(dataInit.parseTaskLine(args,0));
+        return employeeService.getEmployeeTasks(args.split(",")[3]);
     }
 
     public void getTask(String args){
