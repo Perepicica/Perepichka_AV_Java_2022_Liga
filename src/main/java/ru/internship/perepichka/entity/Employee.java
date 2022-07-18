620d7ec3d699d0d27096301a2ad42c6f94865634
perepicica <sashika25@mail.ru>
2022-07-18 11:54:15 +0300

    fetchMode to solve n+1 problem

--- src/main/java/ru/internship/perepichka/entity/Employee.java
+++ src/main/java/ru/internship/perepichka/entity/Employee.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -22,9 +24,9 @@ public class Employee {
     private String name;
 
     @OneToMany(mappedBy = "employee",
-            cascade = CascadeType.ALL,
-            fetch = FetchType.LAZY)
+            cascade = CascadeType.ALL)
     @JsonManagedReference
+    @Fetch(FetchMode.JOIN)
     private List<Task> tasks;
 
     public Employee(long id, String name){
